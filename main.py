4597c042d33c38c220c5d49ffc5b35433f05c51c
devpureza <devpureza@gmail.com>
2025-02-19 19:21:13 -0300

    feat: melhoria na interface do Alô Gordão

---
+++ main.py
@@ -0,0 +1,31 @@
+import streamlit as st
+
+# Configuração da página
+st.set_page_config(
+    page_title="Alô Gordão!",
+    page_icon="🍔",
+    layout="wide"
+)
+
+# Título e introdução
+st.title("Alô Gordão! 🍔")
+
+st.write("Bem-vindo à nossa página!")
+
+# Conteúdo principal
+st.markdown("""
+       Uma página simples com uma mensagem calorosa para nosso gordão!
+    
+    #### O que oferecemos:
+    * Doação de sets 💝
+    * Mortes em avalon 🤗
+    * Rhandim pistola💪
+""")
+
+# Adiciona um separador
+st.divider()
+
+# Adiciona um botão interativo
+if st.button("Clique aqui para uma surpresa! 🎉"):
+    st.balloons()
+    st.success("Você é incrível do jeito que é!")
